e39456e38d033a05d29516995bba6804486ac8ce
siddikur-dev <siddikur.dev@gmail.com>
2025-10-30 21:19:55 +0600

    onnect MongoDB , Recap Post API

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 3000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//
+//
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.rfkbq1n.mongodb.net/?appName=Cluster0`;
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+async function run() {
+  try {
+    const productsCollection = client.db("products_DB").collection("products");
+    // Connect the client to the server	(optional starting in v4.7)
+
+    // post product db
+    app.post("/products", async (req, res) => {
+      const newProduct = req.body;
+      const result = await productsCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    // delete product from db
+    app.delete("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const product = { _id: new ObjectId(id) };
+      const result = await productsCollection.deleteOne(product);
+      res.send(result);
+    });
+
+    // get product from db
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Deal Craft Server Is Running!");
